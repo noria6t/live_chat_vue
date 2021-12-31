25e6b029f8af4681a263879d5bcb2afff3a27b2e
noria6t <takahatanoria@gmail.com>
2021-12-31 11:07:19 +0900

    first commit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2>ログイン</h2>
+  <form @submit.prevent="login">
+    <input type="email" required placeholder="メールアドレス" v-model="email">
+    <input type="password" required placeholder="パスワード" v-model="password">
+    <div class="error">{{ error }}</div>
+    <button>ログインする</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+import setItem from '../auth/setItem'
+
+export default {
+  emits: ['redirectToChatRoom'],
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+    methods: {
+    async login() {
+      try {
+        this.error = null
+
+        const res = await axios.post('#{VUE_CHAT_APP_API_BASE_URL}/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+          }
+        )
+        if (!res) {
+          throw new Error('メールアドレスかパスワードが違います')
+        }
+
+        if (!this.error) {
+          setItem(res.headers, res.data.data.name)
+          this.$emit('redirectToChatRoom')
+        }
+
+        console.log({ res })
+
+        return res
+      } catch (error) {
+        console.log({ error })
+        this.error = 'メールアドレスかパスワードが違います'
+      }
+    }
+  }
+}
+</script>
